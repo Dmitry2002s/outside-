d273972119ae4f7e8f69459e777b2f1de38ddcbf
Дмитрий Ткаченко <CKOPIIUOH.1@yandex.ru>
2021-07-31 22:13:13 +0300

    completed

--- 2021_07_31_ Room area/2021_07_31_ Room area/Source.cpp
+++ 2021_07_31_ Room area/2021_07_31_ Room area/Source.cpp
@@ -2,19 +2,7 @@
 
 using namespace std; 
 
-/*bool print(char a)
-{
-	for (int i = 0; i < 12; i++)
-	{
-		for (int k = 0; k < 12; k++)
-		{
-			cout << a[i][k];
-		}
-		cout << endl;
-	}
-}*/
-
-int size(int n, int a, int b, char field[12][12], int s = 0)
+int size(int n, int a, int b, char field[10][10], int s = 0)
 {
 	if (a<0 || a>n ||b<0 || b>n)
 	{
@@ -35,38 +23,27 @@ int size(int n, int a, int b, char field[12][12], int s = 0)
 int main()
 {
 	int n = 0; 
-	cout << "enter the room size " << endl;
 	cin >> n; 
 	if (3 > n || 10 < n)
 	{
 		return -1;
 	}
 	
-	char field[12][12];
-	for (int i = 0; i < n; i++)
-	{
-		for (int k = 0; k < n; k++)
-		{
-			cout << field[i][k];
-		}
-		cout << endl; 
-	}
-	cout << "enter the room parameters " << endl; 
+	char field[10][10];
 	for (int i = 0; i < n; i++)
 	{
 		for (int k = 0; k < n; k++)
 		{
 			cin >> field[i][k];
 		}
-		cout << endl;
 	}
 	int a = 0; 
 	int b = 0;
-	cout << "enter the coordinates of the room" << endl; 
 	cin >> a >> b; 
 	a -= 1;
 	b -= 1;
 
-	cout << "the size of the room is equal to  " << size(n,a,b,field) << endl;
+	cout  << size(n,a,b,field) << endl;
+	
 	return 0; 
 }
