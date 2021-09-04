1d5a3e7f7f63020ea9c496d6fcaff46146b3fcab
Дмитрий Ткаченко <CKOPIIUOH.1@yandex.ru>
2021-09-04 14:50:56 +0300

    push

---
+++ 2021_09_01_Yandex. Course/2021_09_03_covering a segment with points/Source.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+
+using namespace std; 
+
+bool printArray(int** array,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < 2; k++)
+		{
+			cout << array[i][k] << " ";
+		}
+		cout << endl; 
+	}
+	return true; 
+}
+int** createArray(int n, int c)
+{
+	int** array;
+	array = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		array[i] = new int[c];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < c; k++)
+		{
+			cin >> array[i][k];
+		}
+	}
+	return array; 
+}
+int** swap(int** array, int n, int c, int k) // k -кол-во элементов в массиве , n и c - номера строк которые нужно заменить 
+{
+	int* string; 
+	string = new int [k];
+	for (int i = 0; i < k; i++)
+	{
+		string[i] = array[n][i];
+	}
+	for (int i = 0; i < k; i++)
+	{
+		array[n][i] = array[c][i];
+		array[c][i] = string[i];
+	}
+	return array; 
+}
+
+int** sort(int** array,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int m = i; m < n; m++)
+		{
+			if (array[i][1] > array[m][1])
+			{
+				swap(array, i, m, 2);
+			}
+		}
+	}
+	return array;
+}
+
+int main()
+{
+	int n; 
+	cin >> n; // n - кол-во массивов  
+	int** array = createArray(n, 2);
+	cout << endl; 
+	printArray(array, n);
+	sort(array, n);
+	cout << endl; 
+	printArray(array, n); 
+	cout << endl << endl << endl; 
+	for (int i = 0; i < n; i++)
+	{
+		if (array[i][1] >= array[n - 1][0])
+		{
+			cout << array[i][1]; 
+			break;
+		}
+		
+		else {
+			for (int k = i + 1; k < n; k++)
+			{
+				if (array[i][1] < array[k][0])
+				{
+					cout << array[i][1] << "  ";
+					break;
+				}
+			}
+		}
+	}
+}
