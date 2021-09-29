472a5c7cfd87eefcce0e64ef22203f3f73af36ea
Дмитрий Ткаченко <CKOPIIUOH.1@yandex.ru>
2021-09-30 01:25:55 +0300

    ща

--- 2021_09_09_Homework_2 _class List/2021_09_09_Homework_2 _class List/Source.cpp
+++ 2021_09_09_Homework_2 _class List/2021_09_09_Homework_2 _class List/Source.cpp
@@ -3,19 +3,23 @@
 
 using namespace std; 
 
-
+static int c = 0; 
 struct Subscriber
 {
-	string name ; 
-	string adress ; 
+	 
+	 int order;
+	string name;
+	string adress;
 	string number;
-	Subscriber(string a = "Name", string b = "adress", string c = "000000") :
-		name(a), adress(b), number(c) {}
+	Subscriber(int x = c++ , string a = "Name", string b = "adress", string c = "000000") :
+		order(x) ,name(a), adress(b), number(c) {}
 	
-};
+}; 
 
-std::ostream& operator << (ostream &cout , const Subscriber& S)
+
+std::ostream& operator << (ostream &cout , const Subscriber& S) 
 {
+	cout << S.order << endl; 
 	cout << "Name - " << S.name    <<endl;
 	cout << "Adress - " << S.adress<<endl;
 	cout << "Number - " << S.number<<endl;
@@ -23,25 +27,31 @@ std::ostream& operator << (ostream &cout , const Subscriber& S)
 	return cout;
 }
 
-std::istream& operator >> (istream &cin, const Subscriber& S)
+std::istream& operator >> (istream &cin, Subscriber& S)
 {
-	cin >> S.name >> S.adress >> S.number;
+	cout << "enter in the order of 'name' 'address' 'number' " << endl; 
+	cin >> S.name;
 
-	if (!cin)
-	{
-		Subscriber s;
-	}
+	cin >> S.adress;
+	cin >> S.number;
 	
 	return cin ;
 }
 
 
+
 int main()
 {
+	const int c = 0; 
 	setlocale(LC_ALL, "Russian");
 	Subscriber k;
-	cout << k; 
-	cin >> k; 
-	cout << k; 
+	Subscriber k1;
+	Subscriber k2;
+	Subscriber k3;
+	list<Subscriber> l1;
+
+	
+	cout << k << k1 << k2 << k3 ; 
+	
 }
 
