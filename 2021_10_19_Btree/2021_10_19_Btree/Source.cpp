b65e4ee0e9cdb3227b48eee858cca4fc1cf7dbcf
Дмитрий Ткаченко <CKOPIIUOH.1@yandex.ru>
2021-10-20 01:33:39 +0300

    BThree #1 completed

--- 2021_10_19_Btree/2021_10_19_Btree/Source.cpp
+++ 2021_10_19_Btree/2021_10_19_Btree/Source.cpp
@@ -66,6 +66,57 @@ BNode* leftmost(BNode *p)
 		return result; 
 	}
 }
+bool branchesNot(BNode* p)
+{
+	BNode* k = p; 
+	while (k != nullptr)
+	{
+		if (k->left != nullptr && k->right != nullptr)
+		{
+			return false; 
+		}
+		else 
+		{
+			if (k->left != nullptr)
+			{
+				branchesNot (k->left); 
+			}
+			if (k->right != nullptr)
+			{
+				branchesNot (k->right); 
+			}
+		}
+		return true; 
+	}
+}
+BNode* leftmost_sheet(BNode* p)
+{
+	BNode* left = p; 
+	if (left->left != nullptr)
+	{
+		left = leftmost_sheet(left->left);
+	}
+	else if(left->right != nullptr)
+	{
+		left = leftmost_sheet(left->right);
+	}
+		return left;
+	
+}
+BNode* left2_sheet(BNode* p)
+{
+	BNode* roat = p; 
+	BNode* second = p; 
+	BNode* result  = p;
+	BNode* sheet = leftmost_sheet(p);
+	while (!branchesNot(second->left))
+	{
+		second = second->left; 
+	}
+	result = leftmost_sheet(second->right);
+	return result;
+	
+}
 bool del(BNode* del, BThree d)
 {
 	return 0;
@@ -89,6 +140,41 @@ bool delete_leftmost(BThree d)
 	}
 	return true; 
 }
+bool delete_leftmost_sheet(BThree d)
+{
+	BNode* del = leftmost_sheet(d.root);
+	BNode* prev = d.root;
+	while (prev->left != del && prev->right != del)
+	{
+		if (prev->left != nullptr)
+		{
+			prev = prev->left;
+		}
+		else if (prev->right != nullptr)
+		{
+			prev = prev->right;
+		}
+	}
+	if (prev->left == del)
+	{
+		delete del;
+		prev->left = nullptr; 
+	}
+	else
+	{
+		delete del; 
+		prev->right= nullptr; 
+	}
+	
+	return true; 
+}
+bool add_leftmost(int data,BNode* r )
+{
+	BNode* NEW = new BNode(data) ;
+	BNode* prev = leftmost(r);
+	prev->left = NEW; 
+	return true; 
+}
 int main()
 {
 	BNode* p6 = new BNode(6);
@@ -106,6 +192,19 @@ int main()
 	cout << leftmost(p->root)->data << endl;
 	delete_leftmost(p->root); 
 	p->print();
+	cout << " test leftmost_sheet " << leftmost_sheet(p->root)->data << endl; 
+	add_leftmost(123, p->root);
+	cout << "test add_leftmost" << endl; 
+	p->print();
+	cout << " test leftmost_sheet " << leftmost_sheet(p->root)->data << endl; 
+	cout << "test delete_leftmost_sheet"; 
+	delete_leftmost_sheet(p->root);
+	p->print();
+	delete_leftmost_sheet(p->root);
+	p->print();
+	delete_leftmost_sheet(p->root);
+	p->print();
+	cout  << left2_sheet(p->root)->data;
 	return EXIT_SUCCESS; 
 
 }
