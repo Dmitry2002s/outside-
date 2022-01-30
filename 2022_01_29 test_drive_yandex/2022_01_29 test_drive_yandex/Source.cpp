3990cc4a3b465346ae61ca97bf03663516cf94a5
Дмитрий Ткаченко <st086996@student.spbu.ru>
2022-01-31 01:37:49 +0300

    чиво блин

--- 2022_01_29 test_drive_yandex/2022_01_29 test_drive_yandex/Source.cpp
+++ 2022_01_29 test_drive_yandex/2022_01_29 test_drive_yandex/Source.cpp
@@ -1,66 +1,96 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string max(string& str1, string& str2,string result ="\0", int i = 0)
+bool noMore(string test, int k, char num)
+{
+	while (test[k] != '\0')
+	{
+		if (test[k] < num)
+			return false; 
+		k++;
+	}
+	return true; 
+}
+bool more(string str1, string str2,bool result ="\0", int i = 0)
 {
 	if (str1 == str2)
-		return "\0";
-	if (str1[i] == ('\0'||'X'))
+		return false;
+	else if (str1[i] == 'X' && str2 != "X")
+		return false; 
+	else if (str1[i] == '\0')
 	{
-		result = str2;
-		str2 = "X";
+		if (str2[i] != '0' && noMore(str2, i, str2[0]) ) //str2[i]>=str2[0])
+				return false;
+		result = true;
 	}
-	else if (str2[i] == ('\0' || 'X'))
+		
+	else if (str2[i] == '\0' )
 	{
-		result = str1;
-		str1 = "X";
+
+		if (str1[i] != '0' && noMore(str1, i, str1[0]) ) // str1[i]>=str1[0])
+			return true;
+		result = false;
 	}
 	else if (str1[i] > str2[i])
 	{
-		result = str1;
-		str1 = "X";
+		result = true;
 	}
 	else if (str1[i] < str2[i])
 	{
-		result = str2;
-		str2 = "X";
+		result = false;
 	}
 	else
 	{
-		result =  max(str1, str2, result, ++i);
+		result =  more(str1, str2, result, ++i);
 	}
-	if (result == "X")
-		return "\0";
 	return result;
 }
-string number(string*& mas)
+string number(string*& mas,int countlines) // выводит конечный результат 
 { 
 	string result;
-	for(int i = 0 ; i < 100 ; i++ ) 
-		if (mas[i] != "X")
+	 //Номер максимального элемента. 
+	for (int m = 0; m < countlines; m++)
+	{
+		int i = 0;
+
+		int N = 0;
+		while (mas[i] != "\0")
 		{
-			string lmax = mas[i];
-			for (int j = 0; j < 100; j++)
+			if (mas[i] != "X")
 			{
-				if (i != j && mas[j] != "")
+				if (N != i && mas[i] != "")
 				{
-					lmax = max(lmax, mas[j]);
-					
+					if (more(mas[N], mas[i]) == false)
+						N = i;
+					//lmax = max(lmax, mas[j]);
 				}
+
+				//result += lmax;
 			}
-			result += lmax;
+			i++;
 		}
+
+		if (mas[N] == "X")
+		{
+
+		}
+		else {
+			result += mas[N];
+			mas[N] = 'X';
+		}
+	}
 	return result; 
 }
 
 int main()
 {
 	string* mas = new string[100];
-	int i = 0;
+	int countlines = 0;
 	while (cin)
 	{
-		getline(cin, mas[i]);
-		i++;
+		getline(cin, mas[countlines]);
+		countlines++;
 	}
-	cout << number(mas);
+
+	cout  << number(mas, countlines);
 }
