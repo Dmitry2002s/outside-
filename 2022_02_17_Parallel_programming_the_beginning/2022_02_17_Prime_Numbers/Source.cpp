3883381915a0cd5b633b708faec217943d78c6b3
Дмитрий Ткаченко <st086996@student.sbpu.ru>
2022-02-28 00:47:34 +0300

    3 completed

--- 2022_02_17_Parallel_programming_the_beginning/2022_02_17_Prime_Numbers/Source.cpp
+++ 2022_02_17_Parallel_programming_the_beginning/2022_02_17_Prime_Numbers/Source.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <omp.h>
 #include <math.h>
+#include <cmath>
 
 using namespace std; 
 
@@ -22,7 +23,36 @@ bool Prime(int number)
 	}
 	return true; 
 }
+bool Prime_multi(int number)
+{
+	if (number % 2 == 0)
+		return false;
 
+	if (number % 3 == 0 && number != 3)
+		return false;
+#pragma omp parallel sections
+	{
+
+			for (int i = 1; (6 * i - 1) * (6 * i - 1) <= number; i++)
+			{
+#pragma omp section
+				{
+					if (number % (6 * i - 1) == 0)
+					return false;
+				}
+#pragma omp section
+				{
+					if (number % (6 * i + 1) == 0)
+						return false;
+				}
+			}
+	
+
+			
+		
+	}
+	return true;
+}
 int Prime_numbers(int n)
 {
 	int count = 0; 
@@ -33,6 +63,31 @@ int Prime_numbers(int n)
 	}
 	return count; 
 }
+int Prime_numbers_multi(int n)
+{
+	int count = 0;
+#pragma omp parallel sections(+:count)
+	{
+#pragma omp section
+
+		{		for (int i = 5; i < n; i += 4)
+			{
+				if (Prime(i))
+					count++;
+			}
+		}
+#pragma omp section
+		{
+			for (int i = 3; i < n; i += 4)
+			{
+				if (Prime(i) == true)
+					count++;
+			}
+		}
+	}
+	return count;
+}
+
 
 
 int main()
@@ -40,6 +95,14 @@ int main()
 	int n; 
 	cin >> n; 
 
-	cout << Prime_numbers(n);
+	double m = omp_get_wtime();
+	cout << Prime_numbers_multi(n) << endl;
+	cout << "Time couple cores = " << (omp_get_wtime() - m) << endl;
+
+	double t = omp_get_wtime();
+	cout << Prime_numbers(n) << endl; 
+	cout << "Time one core = " << ( omp_get_wtime() - t );
+
+	
 	return EXIT_SUCCESS; 
 }
