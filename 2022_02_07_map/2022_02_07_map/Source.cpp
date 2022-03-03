b6546464a43a59431758d86dae505e7b19aa952b
Дмитрий Ткаченко <st086996@student.spbu.ru>
2022-03-03 16:20:23 +0300

    map completed

--- 2022_02_07_map/2022_02_07_map/Source.cpp
+++ 2022_02_07_map/2022_02_07_map/Source.cpp
@@ -26,7 +26,6 @@ void question1()
 		{
 			++numbers[number];
 		};
-
 	}
 	Print(numbers);
 }
@@ -35,58 +34,60 @@ map<int, int> question2()
 	map<int, int> res;
 	int arg, result;
 	int q;
-
-	while (cin >> arg >> result)
+	cin >> q; 
+	int i = 0; 
+	while (i < q)
 	{
+		cin >> arg >> result; 
 		res[arg] = result;
+		i++; 
 	}
 	return res;
-}
+} // к сдаче 
 void question3()
 {
-	map<int, int> numbers;
+	//map<int, int> numbers;
 	int num;
 	int data;
 	cout << "Define the function" << endl;
-	while (cin >> num >> data)
+	/*while (cin >> num >> data)
 	{
 		numbers[num] = data;
-	}
+	}*/
+	map<int, int> numbers = question2();
 	cout << "enter a number for the function" << endl;
 	int N;
-	cin.clear();
-	cin >> N;
-	if (numbers.count(N) != 0)
-		cout << numbers[N] << endl;
-	else
-		cout << N << endl;
+	while (cin>>N)
+	{
+		if (numbers.count(N) != 0)
+			cout << numbers[N] << endl;
+		else
+			cout << N << endl;
+	}
 }
 template<typename T, typename N>
 map<T, N> question4(map<T, N> x, map<T, N> y)  // объединение отображений ( отображения считаются непересекающимися) 
 {
 	map<T, N> result;
 	for (auto p : x)
-		result[p.first] = p.second;
+		result[p.first] = p.second; 
 	for (auto p : y)
-		result[p.first] = p.second;
+		result[p.first] += p.second; 
 	return result;
 }
 
-
-template<typename T, typename N>
-map<T, N> question5(map<T, N> first, map<T, N> two)
-{
-	map <T, T> result;
-	for (auto x : first)
-		for (auto y : two)
-		{
-			if (y.first == x.second)
-				result[x.first] = y.second;
-			else
+/*else
 			{
-				result[x.first] = x.second; 
-			}
-		}
+				result[x.first] = x.second;
+				result[y.first] = y.second;
+			}*/
+template<typename T, typename N, typename S>
+map<T, S> question5(map<T, N> first, map<N, S> two)
+{
+	map <T, S> result;
+	
+	for (auto x : first)
+			result[x.first] = two[x.second];
 	return result;
 }
 int main()
@@ -95,7 +96,8 @@ int main()
    //map <string, int> x1 = { {"one" , 4} , {"two" , 8 } };
    //map <string, int> x2 = { {"two" , 6} , {"four" , 9 } };
 
-	map <int, int> x1 = { {1 , 4} , {3,99},{2 , 8}};
+	map <int, int> x1 = { {1 , 4} ,{2 , 8}};
 	map <int, int> x2 = { {4 , 6} , {8 , 9 } };
+	//Print(question4(x1,x2));
 	Print(question5(x1, x2));
 }
