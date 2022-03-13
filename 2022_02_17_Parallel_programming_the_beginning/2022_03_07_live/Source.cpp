2609e207850aadbfbd55750c007089c3f1312dbf
Дмитрий Ткаченко <st086996@student.sbpu.ru>
2022-03-13 12:59:58 +0300

    ф

--- 2022_02_17_Parallel_programming_the_beginning/2022_03_07_live/Source.cpp
+++ 2022_02_17_Parallel_programming_the_beginning/2022_03_07_live/Source.cpp
@@ -1,21 +1,22 @@
+
 #include <iostream>
 #include <omp.h>
 #include <string>
 #include <vector>
 #include <map>
 #include <set>
-using namespace std; 
+using namespace std;
 struct cell
 {
-	int x = -3; 
-	int y = -3; 
-	 bool operator!= (const cell& rv) 
-	 {
-			if(x!=rv.x || y!=rv.y)
-				return false ;
-			return true; 
+	int x = -3;
+	int y = -3;
+	bool operator!= (const cell& rv)
+	{
+		if (x != rv.x || y != rv.y)
+			return false;
+		return true;
 	}
-	
+
 };
 void Print(cell a)
 {
@@ -23,29 +24,29 @@ void Print(cell a)
 }
 int q(cell* a)
 {
-	int result = 0; 
-	cell end; 
+	int result = 0;
+	cell end;
 	end.x = -3;
-	end.y = -3; 
+	end.y = -3;
 	for (int i = 0; a[i] != end; i++)
-		result++; 
-	return result; 
+		result++;
+	return result;
 }
-void Print(map<int ,cell> a)
+void Print(map<int, cell> a)
 {
-	
-	int size = 0; 
+
+	int size = 0;
 	size = a.size();
-	map <int ,cell> ::iterator p;
+	map <int, cell> ::iterator p;
 	cout << "___________" << endl << "list of living cells" << endl;
 	for (int i = 0; i < size; i++)
 		Print(a[i]);
 	cout << "___________" << endl;
 }
- map<int,cell> Live(vector<string> field)
+map<int, cell> Live(vector<string> field)
 {
-	map<int,cell> result;
-	int number = 0; 
+	map<int, cell> result;
+	int number = 0;
 	cell a;
 	int q = field.size();
 	for (int j = 0; j < q; j++)
@@ -53,13 +54,13 @@ void Print(map<int ,cell> a)
 		{
 			if (field[j][i] == '1')
 			{
-				a.x=j ;
+				a.x = j;
 				a.y = i;
 				result[number] = a;
 				number++;
 			}
-			
-			
+
+
 		}
 
 	return result;
@@ -67,43 +68,43 @@ void Print(map<int ,cell> a)
 }
 bool moment(vector<string> field, int i, int j)
 {
-	int neighboor = 0; 
-	vector<string> :: iterator a = field.end();
-	vector<string> :: iterator p = field.begin() + i; 
-	int m_o = -1; 
-	int k_o = -1; 
-	int m_max = 2; 
+	int neighboor = 0;
+	vector<string> ::iterator a = field.end();
+	vector<string> ::iterator p = field.begin() + i;
+	int m_o = -1;
+	int k_o = -1;
+	int m_max = 2;
 	int k_max = 2;
 	if (field[i][j] == '\0')
 		return false;
 	if (i == 0)
-		k_o++; 
+		k_o++;
 	if (j == 0)
 		m_o++;
-	if (p+1 == a)
-		k_max--; 
+	if (p + 1 == a)
+		k_max--;
 	if (field[i][j + 1] == '\0')
-		m_max--; 
+		m_max--;
 	for (int k = k_o; k < k_max; k++)
-		for (int m =m_o; m < m_max; m++)
-			if(k!=0 || m!=0)
+		for (int m = m_o; m < m_max; m++)
+			if (k != 0 || m != 0)
 				if (field[i + k][j + m] == '1')
-					neighboor++; 
+					neighboor++;
 
-	if (2 <= neighboor && neighboor <= 3 && field[i][j]=='1')
+	if (2 <= neighboor && neighboor <= 3 && field[i][j] == '1')
 	{
-		return true; 
+		return true;
 	}
 	else if (neighboor == 3 && field[i][j] == '0')
 	{
-		return true; 
+		return true;
 	}
-	else 
-	return false; 
+	else
+		return false;
 }
 void Print(vector<string> field)
 {
-	
+
 	int q = field.size();
 	for (int j = 0; j < q; j++)
 	{
@@ -111,9 +112,9 @@ void Print(vector<string> field)
 		{
 			cout << field[j][i];
 		}
-		cout << endl; 
+		cout << endl;
 	}
-	cout << endl << endl; 
+	cout << endl << endl;
 }
 
 vector<string> last(vector<string> field)
@@ -121,7 +122,7 @@ vector<string> last(vector<string> field)
 	string a;
 	vector<string> result = field;
 	for (int i = 0; i < field.size(); i++)
-	{		
+	{
 		for (int j = 0; field[i][j] != '\0'; j++)
 		{
 			if (moment(field, i, j) == true)
@@ -129,21 +130,21 @@ vector<string> last(vector<string> field)
 			else
 				a += '0';
 		}
-		result[i] = a; 
-		a="";
+		result[i] = a;
+		a = "";
 	}
-	return result; 
+	return result;
 }
 vector<string> last_multi(vector<string> field)
 {
 	string a;
 	vector<string> result = field;
-#pragma omp parallel sections reduction
+#pragma omp parallel sections 
 	{
 #pragma omp section 
 		{
-			int z = field.size() / 2; 
-			vector<string> result1; 
+			int z = field.size() / 2;
+			vector<string> result1;
 			for (int i = field.size() / 2; i < field.size(); i++)
 			{
 				for (int j = 0; field[i][j] != '\0'; j++)
@@ -153,7 +154,7 @@ vector<string> last_multi(vector<string> field)
 					else
 						a += '0';
 				}
-				result1[i-z] = a;
+				result[i] = a;
 				a = "";
 			}
 		}
@@ -178,25 +179,24 @@ vector<string> last_multi(vector<string> field)
 
 int main()
 {
-	vector <string> field; 
+	vector <string> field;
 	string str;
-	map<int,cell> result;
-	
+	map<int, cell> result;
+
 	/*while (cin >> str)
 	{
 		field.push_back(str);
 	}
 	moment(field, 2, 0);
-	vector <string> new_field; 
+	vector <string> new_field;
 	double t = omp_get_wtime();
 	for (int i = 100; i > 0; i--)
 	{
 		field = last(field);
-		
-	}
-	cout << omp_get_wtime() - t << endl; 
 
-	cout << "test multi " << endl; 
+	}
+	cout << omp_get_wtime() - t << endl;
+	cout << "test multi " << endl;
 	field.clear();
 	*/
 	while (cin >> str)
@@ -204,7 +204,7 @@ int main()
 		field.push_back(str);
 	}
 	double m = omp_get_wtime();
-	for (int i = 2; i > 0; i--)
+	for (int i = 10; i > 0; i--)
 	{
 		field = last_multi(field);
 
@@ -212,8 +212,5 @@ int main()
 	cout << omp_get_wtime() - m << endl;
 
 }
-/*
-
-
-
+/*	
 */
