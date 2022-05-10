9a0df8f5df51ec9e16ac919a12b004cf98157c3c
Дмитрий Ткаченко <st086996@student.sbpu.ru>
2022-05-10 18:59:39 +0300

    fix

--- 2022_02_05_matrix-solution/2022_02_05_matrix-solution/Source.cpp
+++ 2022_02_05_matrix-solution/2022_02_05_matrix-solution/Source.cpp
@@ -1,73 +1,67 @@
 #include <iostream>
 using namespace std;
 
-void divide(double** massive , int number , int q)
+
+void divide(double** massive ,int number , double q, int lenght)
 {
-	for (int i = 0; massive[number][i] != '\0'; i++)
+	for (int i = 0; i < lenght; i++)
 		massive[number][i] = massive[number][i] / q; 
 }
-void subtract(double** massive, int one, int two, double q)
+void subtract(double** massive, int one, int two, double q, int lenght)
 {
-	for (int i = 0; i < sizeof(massive) + 2 != '\0'; i++)
+	for (int i = 0; i < lenght; i++)
 		massive[one][i] = massive[one][i] - q * massive[two][i];
 
 }
-void swap(double** massive, int one, int two)
+void swap(double** massive, int one, int two, int lenght)
 {
-	int k = 0; 
-	for (int i = 0; i<sizeof(massive)+2 != '\0'; i++)
+	double  k = 0; 
+	for (int i = 0; i< lenght; i++)
 	{
 		k = massive[one][i]; 
 		massive[one][i] = massive[two][i];
 		massive[two][i] = k; 
 	}
 }
-void print(double** massive)
+void print(double** massive , int lenght)
 {
 	cout << "_____________________________________________" << endl;
-	for (int i = 0; i < sizeof(massive)+2; i++)
+	for (int i = 0; i < lenght; i++)
 	{
-		for (int k = 0; k < sizeof(massive[i])+2; k++)
-			cout << massive[i][k];
+		for (int k = 0; k < lenght; k++)
+			cout << massive[i][k] << " ";
 		cout << endl; 
 	}
 	cout << "_____________________________________________" << endl;
 }
-void transform(double** massive)
+void transform(double** massive, int lenght )
 {
-	int number_max = 0;
-	int max = 0;
-	for (int i = 0; i<sizeof(massive)+2; ++i)
+	for (int z = 0; z < lenght; z++)
 	{
-		if (max < massive[i][0])
-			max = massive[i][0];
-			number_max = i;
-		
-		}
-	cout << "check 1 " << endl; 
-	print(massive);
-
-	swap(massive,0, number_max);
-	print(massive);
-
-	cout << "check 2 " << endl;
-	for (int k = 0; k < sizeof(massive); k++ )
-	{
-		for (int i = 1+k; i+k < sizeof(massive) + 2; i++)
+		int number_max = z;
+		int max = 0;
+		for (int i = z; i < lenght; ++i)
 		{
-			double q = 0;
-			q = (massive[i+k][k] / massive[0][k]);
-			subtract(massive, i, k, q);
+			if (max < massive[i][0])
+			{
+				max = massive[i][0];
+				number_max = i;
+			}
 		}
-			print(massive);
-	}
-
+		swap(massive, z, number_max, lenght);
+		divide(massive, z, massive[z][z], lenght);
+		for (int k = z+1; k < lenght; k++)
+		{
+			subtract(massive, k, z, massive[k][z], lenght);
+		}
+		
+	} 
+	
 }
-
 
 int main()
 {
-	int n = 5; 
+	int n = 20; 
 	double** massive = new double *[n]; 
 	for (int i = 0; i < n; i++)
 		massive[i] = new double[n];
@@ -79,8 +73,17 @@ int main()
 			massive[i][k] = rand() % 10;
 		}
 	}
-	print(massive);
-	transform(massive);
-	print(massive);
-	delete[] massive; 
+	print(massive, n);
+	/*for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			cin >> massive[i][k];
+		}
+	}*/
+	//, {1, 10 , -1} , {-1 , 1 , 10}};
+	print(massive, n);
+	transform(massive, n);
+	print(massive, n);
+	
 }
